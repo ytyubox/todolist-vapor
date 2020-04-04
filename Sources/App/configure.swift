e54554f9300816563a56f4ecf87d01ff6c6d2688
Tsungyu Yu <ytyubox@gmail.com>
2020-04-04 16:48:25 +0800

    leaf works

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,20 +1,23 @@
 import Vapor
+import Leaf
 import PostgreSQL
 
 /// Called before your application initializes.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
 public func configure(
-    _ config: inout Config,
-    _ env: inout Environment,
-    _ services: inout Services
+	_ config: inout Config,
+	_ env: inout Environment,
+	_ services: inout Services
 ) throws {
-    // Register routes to the router
-    let router = EngineRouter.default()
-    try routes(router)
-    services.register(router, as: Router.self)
-
-    // Configure the rest of your application here
-	
+	// Register routes to the router
+	let router = EngineRouter.default()
+	try routes(router)
+	services.register(router, as: Router.self)
 	try services.register(PostgreSQLProvider())
+	try services.register(LeafProvider())
+	
+	// Configure the rest of your application here
+
+	config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
